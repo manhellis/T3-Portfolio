940bcb9710928a9fddef1f51fe7c6264ed024aa7
manhellis <manh@manhellis.com>
2024-02-22 16:24:33 -0800

    figuring out layout, need help still

--- portfolio-app/src/app/page.js
+++ portfolio-app/src/app/page.js
@@ -5,8 +5,13 @@ export default function Home() {
     return (
         <main className={styles.main}>
             <div className={styles.landing}>
-                <h2 className={styles.h2}>Web Developer</h2>
-                <h1 className={styles.h1}>Manh Ellis</h1>
+                <div className={styles.landingImage}>
+                    <div className={styles.image}></div>
+                </div>
+                <div className={styles.landingText}>
+                    <h2 className={styles.h2}>Web Developer</h2>
+                    <h1 className={styles.h1}>Manh Ellis</h1>
+                </div>
             </div>
         </main>
     );
